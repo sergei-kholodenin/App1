33b9910820ee5e31c2b3ff352d5a28fe7bb881de
sergei <sergeyyak@bk.ru>
2023-02-17 19:11:11 +0300

    Create extractor

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ sg.theme("Black")
 clock = sg.Text("", key="clock")
 label = sg.Text("Type in to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
